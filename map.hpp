14c7b19f4121d7ffd9e8a1bcdae5813d4d911e04
Valentin Lageard <valentin.lageard.pro@gmail.com>
2021-11-18 13:16:19 +0100

    pair and make_pair added

---
+++ map.hpp
@@ -0,0 +1,8 @@
+#ifndef MAP_HPP
+#define MAP_HPP
+
+//TODO: Create BNode template class to manage binary search trees.
+//TODO: Change BNode to RBNode to manage Red Black tree.
+//TODO: Map
+
+#endif
