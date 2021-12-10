69f9438304f413c2bd0913540ec60f07e7b924dd
Valentin Lageard <valentin.lageard.pro@gmail.com>
2021-12-10 14:20:57 +0100

    lexicographical compare debug

--- algorithm.hpp
+++ algorithm.hpp
@@ -31,15 +31,13 @@ bool equal(InputIterator1 first1, InputIterator1 last1, InputIterator2 first2, B
 template <class InputIterator1, class InputIterator2>
 bool lexicographical_compare(InputIterator1 first1, InputIterator1 last1, InputIterator2 first2, InputIterator2 last2) {
 	while (first1 != last1) {
-		if (first2 < first1) {
+		if (first2 == last2 || *first2 < *first1) {
 			return false;
-		} else if (first2 == last2) {
-			return false;
-		} else if (first1 < first2) {
+		} else if (*first1 < *first2) {
 			return true;
 		}
-		first1++;
-		last1++;
+		++first1;
+		++first2;
 	}
 	return (first2 != last2);
 }
@@ -48,15 +46,13 @@ template <class InputIterator1, class InputIterator2, class Compare>
 bool lexicographical_compare(InputIterator1 first1, InputIterator1 last1, InputIterator2 first2, InputIterator2 last2,
 							 Compare comp) {
 	while (first1 != last1) {
-		if (comp(first2, first1)) {
+		if (first2 == last2 || comp(*first2, *first1)) {
 			return false;
-		} else if (first2 == last2) {
-			return false;
-		} else if (comp(first1, first2)) {
+		} else if (comp(*first1, *first2)) {
 			return true;
 		}
-		first1++;
-		last1++;
+		++first1;
+		++first2;
 	}
 	return (first2 != last2);
 }
